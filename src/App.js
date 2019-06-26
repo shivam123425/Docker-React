e235c80a1437482b9ef07206e1f27c4f2e450955
shivam123425 <shivam123425@gmail.com>
2019-06-26 14:22:14 +0530

    Add new text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Yes</p>
+        <p>New change on feature branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
